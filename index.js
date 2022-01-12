993996aa099e081ec320a692d643da82b2c9a068
Shaul6 <95301129+Shaul6@users.noreply.github.com>
2022-01-12 15:53:30 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+document.querySelector(".playButton").addEventListener("click",function() {
+  var randomNumber1 =(Math.floor(Math.random()*6))+1; //1-6
+
+  var randomDiceImage = "dice"+randomNumber1+".png"; //dice1 - dice6
+
+  var randomImageSource = "images/"+ randomDiceImage; //images/dice1.png - images/dice6.png
+
+  var image1 = document.querySelectorAll("img") [0];
+
+  image1.setAttribute("src",randomImageSource );
+
+  var randomNumber2 =(Math.floor(Math.random()*6))+1; //1-6
+
+  var randomDiceImage2 = "images/dice" +randomNumber2+".png"; //dice1 - dice6
+
+  document.querySelectorAll("img")[1].setAttribute("src",randomDiceImage2 );
+
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 wins!";
+  }
+  else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 wins! 🚩";
+  }
+  else {
+    document.querySelector("h1").innerHTML  = "Draw!";
+  }
+});
